e6d8bf1b808455cb908dc1b28d2f3c73dd1ca3b5
xtomyserrax <xtomyserrax@users.noreply.github.com>
2020-05-02 01:59:05 -0300

    Added FieldPreCreationEvent to the API

--- src/main/java/net/sacredlabyrinth/Phaed/PreciousStones/api/events/FieldPreCreationEvent.java
+++ src/main/java/net/sacredlabyrinth/Phaed/PreciousStones/api/events/FieldPreCreationEvent.java
@@ -1,5 +1,6 @@
 package net.sacredlabyrinth.Phaed.PreciousStones.api.events;
 
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
@@ -10,18 +11,28 @@ import net.sacredlabyrinth.Phaed.PreciousStones.field.FieldSettings;
 
 /**
  * @author xtomyserrax
+ *
+ * This event will be called before a field is created so you can cancel it.
+ * getPlayer() will return the Player who placed the Field block.
+ * getFieldSettings() will return the settings of the Field trying to be created.
+ * setFieldSettings(FieldSettings) will let you change the actual Field Settings.
+ * getBlock() will return you the Field block.
+ * setBlock(Block) will let you change the Field block
+ * setCancelled(boolean) will let you cancel the event.
  */
 
 public class FieldPreCreationEvent extends PlayerEvent implements Cancellable {
 	private static final HandlerList handlers = new HandlerList();
 
 	private FieldSettings fieldSettings;
+	private Block block;
 
 	private boolean isCancelled = false;
 
-	public FieldPreCreationEvent(Player player, FieldSettings fieldSettings) {
-		super(player); // Player who placed the field isnt always the owner. (Check flag NO_OWNER)
-		this.fieldSettings = fieldSettings; // You wont get a Field because it hasnt been created yet.
+	public FieldPreCreationEvent(Player player, FieldSettings fieldSettings, Block block) {
+		super(player);
+		this.fieldSettings = fieldSettings;
+		this.block = block;
 	}
 
 	public static HandlerList getHandlerList() {
@@ -34,6 +45,14 @@ public class FieldPreCreationEvent extends PlayerEvent implements Cancellable {
 
 	public void setFieldSettings(FieldSettings fieldSettings) {
 		this.fieldSettings = fieldSettings;
+	}
+	
+	public Block getBlock() {
+		return this.block;
+	}
+
+	public void setBlock(Block block) {
+		this.block = block;
 	}
 
 	public boolean isCancelled() {
